e07c85ba8a6119c7cd7653afa316a2d43df22260
duridev <duridev@outlook.com>
2024-12-07 21:01:05 -0300

    formato peso chileno precios body

--- src/components/Guitarra.vue
+++ src/components/Guitarra.vue
@@ -10,7 +10,9 @@
 
     defineEmits(['agregar-carrito'])
 
-
+    const formatCurrency = (value) => {
+        return new Intl.NumberFormat('es-CL', { style: 'currency', currency: 'CLP' }).format(value).replace('CLP', '').trim();
+    }
 
 </script>
 
@@ -27,7 +29,7 @@
         <div class="col-8">
             <h3 class="text-black fs-4 fw-bold text-uppercase">{{ guitarra.nombre }}</h3>
             <p>{{ guitarra.descripcion }}</p>
-            <p class="fw-black text-primary fs-3">${{ guitarra.precio }}</p>
+            <p class="fw-black text-primary fs-3">{{ formatCurrency(guitarra.precio) }}</p>
             <button 
                 type="button" 
                 class="btn btn-dark w-100 "
